18978129f771e83013daa7340879d9193115dcc6
sebassdf <se.nunezm@duocuc.cl>
2024-07-09 15:54:12 -0400

    3.3.1

--- juegos/urls.py
+++ juegos/urls.py
@@ -5,4 +5,12 @@ from . import views
 urlpatterns = [
     path('index', views.index, name='index'),
 
+    path('crud', views.crud, name='crud'),
+    path('juegosAdd', views.juegosAdd, name='juegosAdd'),
+    path('juegosDel', views.juegosDel, name='juegosDel'),
+    path('juegosEdit', views.juegosEdit, name='juegosEdit'),
+    
+
+
 ]
+
--- juegos/views.py
+++ juegos/views.py
@@ -8,3 +8,49 @@ def index(request):
     juegos=Juego.objects.all()
     context={"juegos":juegos}
     return render(request, 'juegos/index.html', context)
+
+def crud(request):
+    juegos= Juego.objects.all()
+    context={"juegos":juegos}
+    return render (request, 'juegos/juegos_list.html', context)
+
+def juegosAdd(request):
+    if request.method is not "POST":
+        categorias=Categoria.objects.all()
+        context={'categorias':categorias}
+        return render (request, 'juegos/juegos_add.html', context)
+    else:
+        id = request.POST ["id"]
+        nombre = request.POST ["nombre"]
+        precio = request.POST ["precio"]
+        descripcion = request.POST ["descripcion"]
+        id_categoria = request.POST ["id_categoria"]
+        stock = request.POST ["stock"]
+        activo = "1"
+
+        objCategoria=Categoria.objects.get(id_categoria = categoria)
+        obj=Juego.objects.create(id=id,
+                                 nombre=nombre,
+                                 precio=precio,
+                                 descripcion=descripcion,
+                                 id_categoria=objCategoria,
+                                 stock=stock,
+                                 activo=1)
+        obj.save()
+        context= {'mensaje' :"Datos guardados." }
+        return render (request, 'juegos/juegos_add.html', context)
+    
+
+
+    
+def juegosDel(request):
+    if request.method is not "POST":
+        categorias=Categoria.objects.all()
+        context={'categorias':categorias}
+        return render (request, 'juegos/juegos_list.html', context)
+    
+def juegosEdit(request):
+    if request.method is not "POST":
+        categorias=Categoria.objects.all()
+        context={'categorias':categorias}
+        return render (request, 'juegos/juegos_edit.html', context)
